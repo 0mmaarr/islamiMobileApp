95afedffdf24ec54e9c362cfc503e605c46311f6
0mmaarr <omarashraf10000@gmail.com>
2024-08-14 07:39:15 +0300

    feat: adding chapter details logic

--- lib/ui/chapter_details/chapter_details.dart
+++ lib/ui/chapter_details/chapter_details.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/theme/MyTheme.dart';
+import 'package:islami/ui/chapter_details/verse_content.dart';
 
 import '../ui_utilize.dart';
 
-class ChapterDetails extends StatelessWidget {
+class ChapterDetails extends StatefulWidget {
   static const String routeName = 'chapter-details';
 
+  @override
+  State<ChapterDetails> createState() => _ChapterDetailsState();
+}
+
+class _ChapterDetailsState extends State<ChapterDetails> {
+  List<String> verses = [];
   @override
   Widget build(BuildContext context) {
     var args =
         ModalRoute.of(context)?.settings.arguments as DetailedChapterArgs;
+
+    void readFileData(int fileIndex) async {
+      String fileContent =
+          await rootBundle.loadString("assets/files/${fileIndex}.txt");
+      List<String> lines = fileContent.trim().split('\n');
+      setState(() {
+        verses = lines;
+      });
+    }
+
+    if (verses.isEmpty) {
+      readFileData(args.suratarteb);
+    }
+
     return Stack(children: [
       Image.asset(getFullPathImage('main_background.jpg')),
       Scaffold(
@@ -21,13 +44,19 @@ class ChapterDetails extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
           margin: EdgeInsets.symmetric(vertical: 35, horizontal: 24),
           elevation: 40,
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              Text('Details');
-            },
-            itemCount: 114,
-          ),
-        ),
+            child: verses.isNotEmpty
+                ? ListView.separated(
+                    itemBuilder: (context, index) {
+                      return VerseContent(verses[index], index);
+                    },
+                    itemCount: verses.length,
+                    separatorBuilder: (context, index) => Container(
+                          height: 1,
+                          width: double.infinity,
+                          color: MyThemeData.lightPrimary,
+                          margin: EdgeInsets.symmetric(horizontal: 64),
+                        ))
+                : Center(child: CircularProgressIndicator())),
       )
     ]);
   }
@@ -36,6 +65,7 @@ class ChapterDetails extends StatelessWidget {
 class DetailedChapterArgs {
   int chapterIndex;
   String chapterTitle;
+  int suratarteb;
 
-  DetailedChapterArgs(this.chapterIndex, this.chapterTitle);
+  DetailedChapterArgs(this.chapterIndex, this.chapterTitle, this.suratarteb);
 }
