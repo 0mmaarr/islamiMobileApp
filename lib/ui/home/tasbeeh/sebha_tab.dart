67beebc688beda09f0e74e0b3c1d77a55df361cf
0mmaarr <omarashraf10000@gmail.com>
2024-08-17 17:09:56 +0300

    feat: adding chapter details logic feat: adding ahadeeth details

--- lib/ui/home/tasbeeh/sebha_tab.dart
+++ lib/ui/home/tasbeeh/sebha_tab.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+import '../../../theme/MyTheme.dart';
+
+class SebhaTab extends StatefulWidget {
   @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  @override
+  int i = 0;
+  double rotationAngle = 45;
+
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.white,
-    );
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AnimatedRotation(
+            duration: Duration(seconds: 1),
+            turns: (rotationAngle + 45) / 36,
+            child: Image.asset('assets/images/body of seb7a.png')),
+      ),
+      Text(
+        'عدد التسبيحات',
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+      ),
+      Container(
+        child: Text(
+          '$i',
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+        //color: MyThemeData.lightPrimary,
+        alignment: Alignment.center,
+        height: 100,
+        width: 80,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: MyThemeData.lightPrimary),
+      ),
+      InkWell(
+        onTap: () {
+          i++;
+
+          setState(() {
+            rotationAngle++;
+          });
+        },
+        child: Container(
+          child: Text(
+            'سبحان الله',
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white),
+          ),
+          //color: MyThemeData.lightPrimary,
+          alignment: Alignment.center,
+          height: 80,
+          width: 160,
+          margin: EdgeInsets.symmetric(vertical: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: MyThemeData.lightPrimary),
+        ),
+      )
+    ]);
   }
 }
+
